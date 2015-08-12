254dda54d387431b99b657dae23de20223549763
Darren Gibbard <dalgibbard@gmail.com>
2015-08-12 21:53:56 +0100

    Added exclusions based on details in each CTX article for XS65

--- exclusions/XS65_excludes.py
+++ exclusions/XS65_excludes.py
@@ -1,3 +1,2 @@
-## Thanks to @deltaprojects for this listing.
 #XS65ESP1 Includes these items already, so these are not required.
-autoexclusions = [ "XS65E001", "XS65E002", "XS65E003", "XS65E005", "XS65E006", "XS65E007", "XS65E008", "XS65E009", "XS65E010" ]
+autoexclusions = [ "XS65E001", "XS65E002", "XS65E003", "XS65E005", "XS65E006", "XS65E007", "XS65E008", "XS65E009", "XS65E010", "XS65E013", "XS65ESP1002", "XS65ESP1004" ]
